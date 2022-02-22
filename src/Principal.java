827dc5ec857f35634268bc4532b2a12088a028c1
EndikaDelValle <dam13.2022@gmail.com>
2022-02-22 10:06:27 +0100

    Funcion Resta

--- src/Principal.java
+++ src/Principal.java
@@ -3,6 +3,9 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Calculadora miCalc= new Calculadora();
+		System.out.println(miCalc.sumar(5, 3));
+		System.out.println(miCalc.restar(5, 3));
 
 	}
 
